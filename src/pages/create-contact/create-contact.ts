a0007ec669c22cc47a135e2f5d8a8a3713304ec4
Andressa Dogado <andressakdogado@gmail.com>
2019-05-02 19:18:34 -0300

    Cadastro de contatos adicionado

--- src/pages/create-contact/create-contact.ts
+++ src/pages/create-contact/create-contact.ts
@@ -17,13 +17,22 @@ import { ContactsProvider } from '../../providers/contacts/contacts';
 export class CreateContactPage {
 
     model: Contact;
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public contactsProvider: ContactsProvider) {
         this.model = new Contact();
-        this.model.name = ‘Novo contato’;
-        this.model.gender = ‘male’;
+        this.model.name = "Novo contato";
+        this.model.gender = "male";
     }
 
-    createContact(){
+    createContact() {
+        var data = { 'name': this.model.name, 'gender':
+        this.model.gender };
+        this.contactsProvider.addContact(data)
+        .then((result: any) => {
+            console.log('Contato criado');
+        })
+        .catch((error: any) => {
+            console.log(error);
+        });
     }
 
     ionViewDidLoad() {
--- src/providers/contacts/contacts.ts
+++ src/providers/contacts/contacts.ts
@@ -3,22 +3,33 @@ import { Injectable } from '@angular/core';
 
 const STORAGE_KEY = 'contacts';
 /*
-  Generated class for the ContactsProvider provider.
+Generated class for the ContactsProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
+See https://angular.io/guide/dependency-injection for more info on providers
+and Angular DI.
 */
 @Injectable()
 export class ContactsProvider {
 
-  constructor(public storage: Storage) {
-    console.log('Hello ContactsProvider Provider');
-  }
+    constructor(public storage: Storage) {
+        console.log('Hello ContactsProvider Provider');
+    }
 
-  getContacts() {
-      return this.storage.get(STORAGE_KEY);
-  }
+    getContacts() {
+        return this.storage.get(STORAGE_KEY);
+    }
 
-  addContact(data) {}
-  
+    addContact(data){
+        return this.getContacts().then(result => {
+            if (result) {
+                data['id'] = result.length + 1;
+                result.push(data);
+                return this.storage.set(STORAGE_KEY, result);
+            } else {
+                data['id'] = 1;
+                return this.storage.set(STORAGE_KEY, [data]);
+            }
+        });
+    }
+
 }
